800b8d0bf48dbc49156e4731ec23d68d609cc417
meltingrabbit <39541984+meltingrabbit@users.noreply.github.com>
2018-08-15 21:26:39 +0900

    db seed

--- test01/db/seeds.rb
+++ test01/db/seeds.rb
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Mdl01.create(name: '鈴木 太郎', age: '20', disc: 'ああああ')
+Mdl01.create(name: '鈴木 次郎', age: '-20', disc: 'あああっだｓあ')
