76cf0b89448a64cd26fbb9fa9ff9f9e6f964f083
meltingrabbit <39541984+meltingrabbit@users.noreply.github.com>
2018-08-15 21:09:52 +0900

    db

--- test01/db/migrate/20180815115410_create_mdl01s.rb
+++ test01/db/migrate/20180815115410_create_mdl01s.rb
@@ -1,6 +1,9 @@
 class CreateMdl01s < ActiveRecord::Migration[5.2]
   def change
     create_table :mdl01s do |t|
+      t.string :name
+      t.integer :age
+      t.text :disc
 
       t.timestamps
     end
