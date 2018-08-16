536a31b4dc2d2c7e4bc47593743e9ab72207f514
meltingrabbit <39541984+meltingrabbit@users.noreply.github.com>
2018-08-16 21:38:52 +0900

    rake db:migrate

--- test01/db/schema.rb
+++ test01/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_16_101617) do
+ActiveRecord::Schema.define(version: 2018_08_16_123820) do
 
   create_table "friends", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_08_16_101617) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "my_id"
   end
 
   create_table "mdl01s", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
